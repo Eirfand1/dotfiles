18d118648cbebf3cb15fa510d8a929a7dffd2cea
Eirfand1 <name.fandi07@proton.me>
2025-05-10 15:27:02 +0000

    make function that copy line

--- init.el
+++ init.el
@@ -163,3 +163,34 @@
     (eshell)))
 
 (global-set-key (kbd "C-c s") #'my/split-and-shell)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(gruber-darker))
+ '(custom-safe-themes
+   '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
+	 default))
+ '(package-selected-packages
+   '(company-go dashboard doom-themes emms exec-path-from-shell
+				flymake-go go-autocomplete gruber-darker-theme
+				impatient-mode lsp-ui pdf-tools typit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+(defun duplicate-line()
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (next-line 1)
+  (yank)
+  )
+(global-set-key (kbd "C-,") 'duplicate-line)
